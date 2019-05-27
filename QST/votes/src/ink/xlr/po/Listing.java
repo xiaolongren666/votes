169fef1296ab3443c530916eef24648bb4790fac
xiaolongren <1315516121@qq.com>
2019-05-27 14:13:34 +0800

    5/27/14:13xlr

---
+++ QST/votes/src/ink/xlr/po/Listing.java
@@ -0,0 +1,44 @@
+package ink.xlr.po;
+
+/**
+ * 选项标题，
+ * 选项数，
+ * 投票人数。
+ * 该登录账户是否已投票。
+ * @author Administrator
+ *
+ */
+public class Listing {
+    private String title; //标题
+    private int optionNum;//选项数量
+    private int voteNum;//投票数量
+    private boolean isVote;
+
+
+    public boolean getIsVote() {
+        return isVote;
+    }
+    public void setIsVote(boolean isVote) {
+        this.isVote = isVote;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getOptionNum() {
+        return optionNum;
+    }
+    public void setOptionNum(int optionNum) {
+        this.optionNum = optionNum;
+    }
+    public int getVoteNum() {
+        return voteNum;
+    }
+    public void setVoteNum(int voteNum) {
+        this.voteNum = voteNum;
+    }
+
+}
---
+++ QST/votes/src/ink/xlr/po/Option.java
@@ -0,0 +1,26 @@
+package ink.xlr.po;
+
+
+/**
+ * 存储每个选项对应的票数。
+ * @author Administrator
+ *
+ */
+
+public class Option {
+    private String option; //选项
+    private int num;//数量
+    public String getOption() {
+        return option;
+    }
+    public void setOption(String option) {
+        this.option = option;
+    }
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+}
